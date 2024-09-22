b2eedd3618bc751b57f7ae541ace748a56c53e7b
deekshaaneja <deekshaaneja@gmail.com>
2024-09-22 14:58:51 +0530

    added finetuning code

--- finetune.py
+++ finetune.py
@@ -259,7 +259,7 @@ def make_supervised_data_module(
 def train():
     global local_rank
 
-    output_dir = "/home/ubuntu/deeksha/db-adtech-vl/qwen2_vl_model/output"
+    output_dir = "qwen2_vl_model/output"
     parser = transformers.HfArgumentParser(
         (ModelArguments, DataArguments, TrainingArguments, LoraArguments)
     )
